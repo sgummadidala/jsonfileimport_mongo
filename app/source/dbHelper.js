f39dfaf8ddb3ad282a93de2ffa0d6572396c4b63
TSL139 <tsl139@MacBook-Pro.local>
2018-07-12 18:42:09 -0400

    code cleanup

--- app/source/dbHelper.js
+++ app/source/dbHelper.js
@@ -7,18 +7,6 @@ class MongoHelper{
         //Initialization stuff
         this.connectionString='mongodb://localhost:27017/test';
     }
-    insertDocuments(db,record,callback) {
-        var collectionName = record["$type"];
-
-        // Get the documents collection
-        var collection = db.collection(collectionName);
-        // Insert some documents
-        collection.insert(record, function(err, result) {
-                assert.equal(err, null);
-                console.log("Inserted into the collection");
-                callback(result);
-        });
-    }
     SaveRecord(collectionName,arrayOfRecords){
         MongoClient.connect(this.connectionString, function(err, db) {
             if(!err){
